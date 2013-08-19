2ac39803496565dc9a90ae5b2d3c65c252530a02
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-08-18 20:28:18 -0700

    youtube id for modal

--- app/helpers/youtube_helper.rb
+++ app/helpers/youtube_helper.rb
@@ -9,4 +9,13 @@ module YoutubeHelper
     end
     %Q{<iframe title="YouTube video player" width="640" height="390" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
   end
+  def parse_youtube_id(youtube_url)
+    if youtube_url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
+    youtube_id
+  end
 end
