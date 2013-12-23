f1be6d4dcecfa7d17354c994e34628b765396f07
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-12-22 18:17:38 -0800

    Better ordering and styling for listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,7 +6,7 @@ class ListingsController < ApplicationController
   # GET /listings
   # GET /listings.json
   def index
-    @listings = Listing.all
+    @listings = Listing.order('created_at DESC').all
 
     respond_to do |format|
       format.html # index.html.erb
