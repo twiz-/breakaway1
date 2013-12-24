c48abbbc5374d48229cda5219dbc0feb2575d69a
Tony Ramirez <tony.michaelramirez@gmail.com>
2013-12-24 12:36:21 -0800

    finishing styling for coaches page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user = User.new
   end
   def coaches
+    @listings = Listing.order("created_at DESC").limit(4)
     @user = User.new
   end
 end
